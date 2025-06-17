b1e4ef2ec69db7199d8962fe86472e06018fd313
Rohit Krishnan <krishr.somu@gmail.com>
2025-06-17 12:38:47 +0530

    project structure implemented

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rohit Krishnan",
+    author_email="krishr.somu@gmail.com",
+    packages=find_packages()
+)
